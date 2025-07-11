7f841755a1db7360346df91268ccf33a1d1b50e2
Hirenlekhadiya09 <hirenlekhadiya@gmail.com>
2025-07-11 17:35:25 +0530

    fix cors error

--- server/server.js
+++ server/server.js
@@ -9,18 +9,18 @@ const app = express();
 
 const allowedOrigins = ['https://spotify-frontend-tutg.onrender.com'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS not allowed for this origin: ' + origin));
-    }
-  },
+
+
+var corsOptions = {
+  origin: ['https://spotify-zov4.onrender.com','http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
+
+app.use(cors(corsOptions));
 
 app.options('*', cors());
 
